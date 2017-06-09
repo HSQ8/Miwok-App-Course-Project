f510448247c3997a7d1721b49b31a01b2ca22f18
HQ <hongsenqin@gmail.com>
2017-06-09 16:04:18 -0500

    updated to add click listeners

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,12 +8,24 @@ public class Word {
     private String eng;
     private String miwok;
     private int pic;
+    private int audio;
     private boolean hasImage = false;
-        public Word(String _eng, String _miwok){
+    private boolean hasAudio = false;
+
+    public Word(String _eng, String _miwok){
         eng = _eng;
         miwok= _miwok;
 
         }
+
+    public Word(String _eng, String _miwok, int _pic, int _audio){
+        eng = _eng;
+        miwok= _miwok;
+        pic = _pic;
+        hasImage = true;
+        hasAudio = true;
+        audio = _audio;
+    }
 
     public Word(String _eng, String _miwok, int _pic){
         eng = _eng;
@@ -22,7 +34,8 @@ public class Word {
         hasImage = true;
     }
 
-
+    public int getAudio(){return audio;}
+    public boolean ishasAudio(){return hasAudio;}
     public String getDefaultTranslation(){ return eng;}
     public String getMiwokTranslation(){return miwok;}
     public int getPic(){ return pic;}
--- app/src/main/java/com/example/android/miwok/WordArrayAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,31 +16,43 @@ import java.util.ArrayList;
  * Created by HQ on 5/31/2017.
  */
 
-public class WordArrayAdapter extends ArrayAdapter<Word>{
+public class WordArrayAdapter extends ArrayAdapter<Word> {
     private int colorid;
-    public WordArrayAdapter(Context context, ArrayList<Word> words, int _colorid){
-        super(context,0,words);
+
+    public WordArrayAdapter(Context context, ArrayList<Word> words, int _colorid) {
+        super(context, 0, words);
         colorid = _colorid;
-        }
+    }
 
     @Override
-    public View getView (int position, View convertView, ViewGroup parent){
-Word word = getItem(position);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Word word = getItem(position);
 
-        if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.listitemlayout,parent,false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.listitemlayout, parent, false);
         }
-ImageView illustration = (ImageView) convertView.findViewById(R.id.wordImage);
-if(word.isHasImage()) {
-    illustration.setImageResource(word.getPic());
-}
-else
-    illustration.setVisibility(View.GONE);
+
+        ImageView illustration = (ImageView) convertView.findViewById(R.id.wordImage);
+        final MediaPlayer pronounciation;
+        if (word.isHasImage()) {
+            illustration.setImageResource(word.getPic());
+        }
+
+        if (word.ishasAudio()) {
+            pronounciation = MediaPlayer.create(getContext(),word.getAudio());
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    pronounciation.start();
+                }
+            });
+
+        }
 
         TextView english = (TextView) convertView.findViewById(R.id.englishview);
         TextView miwok = (TextView) convertView.findViewById(R.id.miwokview);
-english.setBackgroundColor(ContextCompat.getColor(getContext(),colorid));
-        miwok.setBackgroundColor(ContextCompat.getColor(getContext(),colorid));
+        english.setBackgroundColor(ContextCompat.getColor(getContext(), colorid));
+        miwok.setBackgroundColor(ContextCompat.getColor(getContext(), colorid));
 
         english.setText(word.getDefaultTranslation());
         miwok.setText(word.getMiwokTranslation());
